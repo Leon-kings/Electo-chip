ed5f30a453d77a2bc9072b0a766a7a4d9a552c22
Leon-kings <leonakingeneye2002@gmail.com>
2025-03-21 18:39:57 +0200

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,27 @@
-import React from "react";
-import "./App.css";
-import Navbar from "./components/navbar/Navbar";
-import Services from "./pages/services/Services";
-import About from "./pages/about/About";
-import Info from "./pages/information/Info";
-import Footer from "./components/footer/Footer";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/home/Home";
+import React from 'react'
+import './App.css'
+import Navbar from './components/navbar/Navbar'
+import SliderPart from './components/slide/Slider'
+import Services from './pages/services/Services'
+import About from './pages/about/About'
+import Blog from './pages/blog/Blog'
+
+import Contact from './pages/contact/Contract'
+import Info from './pages/information/Info'
+import Footer from './components/footer/Footer'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 export default function App() {
   return (
     <>
-      <div className="w-full">
-        <BrowserRouter>
-          <Navbar />
-          <Routes>
-            <Route element={<Home/> } path="/"/>
-            <Route element={<About/> } path="/894-637.783"/>
-            <Route element={<Services/> } path="/8494-383.90"/>
-          </Routes>
-          <Info />
-          <Footer />
-        </BrowserRouter>
-      </div>
+      <Navbar/>
+      <SliderPart/>
+      <Services/>
+      <About/>
+      <Blog/>
+      <Contact/>
+      <Info/>
+      <Footer/>
     </>
-  );
+  )
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import SliderPart from '../../components/slide/Slider'
+import Services from '../services/Services'
+import About from '../about/About'
+import Blog from '../blog/Blog'
+import Contact from '../contact/Contract'
 
 export default function Home() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className="w-full">
+        <div className="w-full">
+            <SliderPart/>
+        </div>
+        <div className="w-full">
+            <Services/>
+        </div>
+        <div className="w-full">
+            <About/>
+        </div>
+        <div className="w-full">
+            <Blog/>
+        </div>
+        <div className="w-full">
+            <Contact/>
+        </div>
+      </div>
+    </>
   )
 }
